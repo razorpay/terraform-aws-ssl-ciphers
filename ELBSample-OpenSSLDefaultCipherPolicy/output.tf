bb01837a2f429b23c5a52ddc351ab81918bac73d
Dhananjay Balan <dhananjay@cliqz.com>
2018-09-11 13:59:32 +0200

    Module might be attached to a non-existing ELB.
    An ELB resource might not be realised from declaration, i.e count is
    set to 0. In that case the module invocation will fail because of the
    variables for a non existing resource are accessed. This commit
    replaces the output values with empty ones.
    
    For e.g
    
    resource "aws_elb" "myelb" {
      count = "${var.createelb == 0 ? 0 : 1 }"
      name                     = "${var.base_name}-elb"
    }
    
    module "ssl-policy-elb" {
      create = "${var.createelb == 0 ? 0 : 1 }"
      source = "$GH/terraform-aws-ssl-ciphers/ELBSecurityPolicy-TLS-1-2-2017-01"
      name = "public-ssl-policy"
      load-balancer-id = "${element(concat(aws_elb.myelb.*.id, list("")), 0)}"
    }

--- ELBSample-OpenSSLDefaultCipherPolicy/output.tf
+++ ELBSample-OpenSSLDefaultCipherPolicy/output.tf
@@ -1,22 +1,22 @@
 output "id" {
-  value = "${aws_lb_ssl_negotiation_policy.policy.id}"
+  value = "${element(concat(aws_lb_ssl_negotiation_policy.policy.*.id, list("")),0)}"
 }
 
 output "name" {
-  value = "${aws_lb_ssl_negotiation_policy.policy.name}"
+  value = "${element(concat(aws_lb_ssl_negotiation_policy.policy.*.name, list("")),0)}"
 }
 
 output "load-balancer" {
-  value       = "${aws_lb_ssl_negotiation_policy.policy.load_balancer}"
+  value       = "${element(concat(aws_lb_ssl_negotiation_policy.policy.*.load_balancer, list("")),0)}"
   description = "The load balancer to which the policy is attached."
 }
 
 output "lb-port" {
-  value       = "${aws_lb_ssl_negotiation_policy.policy.lb_port}"
+  value       = "${element(concat(aws_lb_ssl_negotiation_policy.policy.*.lb_port, list("")),0)}"
   description = "The load balancer port to which the policy is applied. "
 }
 
 output "attribute" {
-  value       = "${aws_lb_ssl_negotiation_policy.policy.attribute}"
+  value       = "${aws_lb_ssl_negotiation_policy.policy.*.attribute}"
   description = "The SSL Negotiation policy attributes. "
 }
